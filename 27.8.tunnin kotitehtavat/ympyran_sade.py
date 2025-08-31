edc99e8847091ce274449dcc82fb8605a361d1e4
talviti <tiia.talvitie@gmail.com>
2025-08-31 17:41:07 +0300

    27.8. tunnin kotitehtävät.

--- 27.8.tunnin kotitehtavat/ympyran_sade.py
+++ 27.8.tunnin kotitehtavat/ympyran_sade.py
@@ -1,3 +1,4 @@
 import math
-Sade= int(input("Anna sateen mitta"))
+Sade= int(input("Anna säteen mitta"))
 print(f"{math.pi*Sade**2:.5f}")
+#muutin kysymyksen ääkkösiä sisältäväksi, on hieman käyttäjäystävällisempi
