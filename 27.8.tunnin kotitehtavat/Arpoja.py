2c125f0107108caf756d717650f3bcfe704fe685
talviti <tiia.talvitie@gmail.com>
2025-08-31 16:19:56 +0300

    27.8. tunnin kotitehtävät. Tein vaihtoehtoja toteutukselle.

--- 27.8.tunnin kotitehtavat/Arpoja.py
+++ 27.8.tunnin kotitehtavat/Arpoja.py
@@ -3,6 +3,14 @@ random.randrange(0,9)
 print(random.randrange(0,9), random.randrange(0,9), random.randrange(0,9))
 print(random.randrange(1,6), random.randrange(1,6), random.randrange(1,6), random.randrange(1, 6))
 
-
+option1 = random.randint(0,9)
+option2 = random.randint(0,9)
+option3 =random.randint(0,9)
+option4 = random.randint(1,6)
+option5 = random.randint(1,6)
+option6 = random.randint(1,6)
+option7 = random.randint(1,6)
+print(option1, option2, option3)
+print(option4, option5, option6, option7)
 
 
