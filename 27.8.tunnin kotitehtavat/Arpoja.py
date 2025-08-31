dffa9198798530bf31c462a1d1c6daef5733b278
talviti <tiia.talvitie@gmail.com>
2025-08-31 16:08:26 +0300

    27.8. tunnin kotitehtävät

--- 27.8.tunnin kotitehtavat/Arpoja.py
+++ 27.8.tunnin kotitehtavat/Arpoja.py
@@ -1,7 +1,7 @@
 import random
 random.randrange(0,9)
 print(random.randrange(0,9), random.randrange(0,9), random.randrange(0,9))
-print(random.randrange(0,9), random.randrange(0,9), random.randrange(0,9), random.randrange(0, 9))
+print(random.randrange(1,6), random.randrange(1,6), random.randrange(1,6), random.randrange(1, 6))
 
 
 
