e59fa9f8edf1fe5272bfc145c1ba2f71bcf772f7
talviti <tiia.talvitie@gmail.com>
2025-11-18 18:40:45 +0200

    19.11. tunnin kotitehtävät.

--- 19.11. kotitehtävät/vuodenaika.py
+++ 19.11. kotitehtävät/vuodenaika.py
@@ -1,7 +1,20 @@
-kuukaudet = ("tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu")
-kuukausi = int(input("Anna minulle haluamasi kuukauden numero. Kerron sinulle tiedon saatuani, mistä vuodenajasta on kysymys. Huomioithan, että lasken ensimmäiseksi talvikuukaudeksi joulukuun. "))
+kuukaudet = ("tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu",
+             "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu")
+kuukausi = int(input("Anna minulle haluamasi kuukauden numero. Kerron sinulle tiedon saatuani, mistä vuodenajasta on kysymys.\n"
+                     "Huomioithan, että lasken ensimmäiseksi talvikuukaudeksi joulukuun. "))
 vuodenajat = ("Talvi", "Kevät", "Kesä", "Syksy")
 vastaus = (kuukaudet[kuukausi-1])
 print(vastaus)
 #nyt se printtaa sentään oikean kuukauden
+if kuukausi == 1 :
+    print("Talvi")
+if 2<=kuukausi<=4:
+    print("Kevät")
+if 5<=kuukausi<=8:
+    print("Kesä")
+if 9<=kuukausi<=11:
+    print("Syksy")
+if 12== kuukausi:
+    print("Talvi")
+
 
